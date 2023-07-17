5864b711c51f5441700b74f97edef749902b9e15
alpheol <colmenex@gmail.com>
2023-07-16 20:57:52 -0400

    github

---
+++ ramajuan/juan1.js
@@ -0,0 +1,25 @@
+fadsf
+falseasf
+AudioBufferSourceNodesf
+
+debuggerfas
+false
+asf
+AudioBufferSourceNodesf
+debuggerfas
+false
+//create function suma
+function suma(a,b){
+    return a+b;
+    }
+    //create function resta
+    function resta(a,b){
+        return a-b;
+        }
+        //create function multiplicacion
+        function multiplicacion(a,b){
+            return a*b;
+            }
+            //create function division
+            function division(a,b){
+            
